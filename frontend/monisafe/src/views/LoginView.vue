0febeace62bf140fee69ba9511289959e902a14d
GhostYi330 <cx330yeeer@gmail.com>
2024-08-28 21:11:00 +0800

    frontend

---
+++ frontend/monisafe/src/views/LoginView.vue
@@ -0,0 +1,139 @@
+<template>
+    <!-- 登录表单 -->
+    <div class="login-page">
+        <el-card class="login-card">
+
+            <!-- 标题 -->
+            <div class="header">
+                <img src="@/assets/logo.png" alt="Logo" class="logo" />
+                <h1>银行监控告警系统</h1>
+            </div>
+
+            <!-- 登录组件 -->
+            <el-form :model="loginForm" @submit.prevent="login" label-position="top">
+                <el-form-item label="用户名" prop="username">
+                    <el-input v-model="loginForm.username" placeholder="请输入用户名" class="short-input"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="password">
+                    <el-input v-model="loginForm.password" type="password" placeholder="请输入密码" class="short-input"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" class="login-button" @click="login">登录</el-button>
+                </el-form-item>
+            </el-form>
+            <div class="auth-buttons">
+                <router-link to="/user/register" class="auth-button">注册</router-link>
+                <span class="space"> </span>
+                <router-link to="/user/forget_password" class="auth-button">忘记密码</router-link>
+            </div>
+        </el-card>
+    </div>
+</template>
+  
+
+<script>
+import axios from 'axios';
+import { ElMessage } from 'element-plus'
+
+export default {
+    data() {
+        return {
+            loginForm: {
+                username: '',
+                password: '',
+            },
+        };
+    },
+    methods: {
+        login(){
+		    axios.post('/user/login',this.loginForm)
+		        .then(response => {
+		            console.log(response.data);
+		  		      if(response.data.code === 0) {
+                    ElMessage.success("登录成功");
+                    sessionStorage.setItem("token", response.data.data);
+                    sessionStorage.setItem("isLogin", 1);
+                    this.$router.push({ path: '/user/home' });
+                } else {
+                    ElMessage.error(response.data.message || "登录失败");
+                }
+                })
+		        .catch(error => {
+		            console.error('请求发送失败', error);
+                    ElMessage.error('网络错误，请稍后重试');
+		    });
+	    }
+    },
+};
+</script>
+  
+
+<style scoped>  
+.login-card {
+    width: 400px;
+    padding: 20px;
+    background-color: rgba(255, 255, 255, 0.6); 
+    border-radius: 8px;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+}
+  
+.header {
+    text-align: center;
+    margin-bottom: 20px; 
+}
+  
+.logo {
+    max-width: 100px;
+    height: auto;
+    margin-bottom: 10px;
+}
+  
+.header h1 {
+    font-family: 'Arial', sans-serif;
+    font-size: 36px;
+    color: #333;
+}
+  
+.short-input {
+    width: 400px;
+}
+  
+.form-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+  
+.form-item {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+}
+  
+.login-button {
+    display: block;
+    width: 30%;
+    margin: 0 auto;
+    border-radius: 25px;
+}
+  
+.auth-buttons {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+}
+  
+.auth-button {
+    color: #007bff;
+    text-decoration: none;
+    font-size: 16px;
+}
+  
+.auth-button:hover {
+    text-decoration: underline;
+}
+  
+.space {
+    flex-grow: 1;
+}
+</style>
